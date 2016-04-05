4f05f98d3c267a5fc98649c1bf112ad76447935d
Samuel Dorado <samuel.dorado@sngular.team>
2016-04-05 12:24:16 +0200

    demo files deleted

--- app/components/demoComponent/demoComponentDirective.js
+++
@@ -1,8 +0,0 @@
-angular.module('card-slider')
-
-.directive('demoComponent', [function() {
-        return {
-            restrict: 'E',
-            templateUrl: 'components/demoComponent/demoComponent.html'
-        };
-    }]);
